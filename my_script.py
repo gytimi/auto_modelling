5a1b8c60bbca94bb58c901c427f8cc92382687cb
György Tímea <gy.timi44@gmail.com>
2019-04-21 15:51:18 +0200

    Little changes in auto-pipeline

--- my_script.py
+++ my_script.py
@@ -1,12 +1,13 @@
 import auto_modelling
 my_data = auto_modelling.my_reader('DataSet_Hitelbiralat_Prepared.csv', separ=';')
-#my_data['TARGET_LABEL_GOOD'] = 1 - my_data['TARGET_LABEL_BAD']
+my_data['TARGET_LABEL_GOOD'] = 1 - my_data['TARGET_LABEL_BAD']
 #target = 'Sex_Female'
-target = auto_modelling.choose_target(my_data)
+#target = auto_modelling.choose_target(my_data)
 #target = 'active_days'
+target = 'TARGET_LABEL_GOOD'
 regression, classification = auto_modelling.guess_goal(my_data, target)
 input_vars = auto_modelling.to_pure_numbers(my_data)
 my_data = auto_modelling.missing_value_handle(my_data)
 train_df, test_df = auto_modelling.my_train_test_split(my_data)
 test_df = auto_modelling.modelling(my_data, regression, classification, train_df, test_df, input_vars, target)
-print(auto_modelling.my_evaulation(test_df, target, regression, classification))
+print(auto_modelling.my_evaluation(test_df, target, regression, classification))
